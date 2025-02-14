8df7c36f0c8515e5b6a4c5563a46c5c941015732
AvansIvar <igm.schreutelkamp@student.avans.nl>
2025-02-14 15:16:21 +0100

    Initial

---
+++ binge-queuer/binge-queuer/binge_queuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  binge_queuerApp.swift
+//  binge-queuer
+//
+//  Created by Ivar Schreutelkamp on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct binge_queuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ binge-queuer/binge-queuerTests/binge_queuerTests.swift
@@ -0,0 +1,17 @@
+//
+//  binge_queuerTests.swift
+//  binge-queuerTests
+//
+//  Created by Ivar Schreutelkamp on 14/02/2025.
+//
+
+import Testing
+@testable import binge_queuer
+
+struct binge_queuerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
